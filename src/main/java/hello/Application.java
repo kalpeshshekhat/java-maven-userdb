d9648d189cffe13a13a4e331b9953851b3f694f8
root <root@jenkins.lrn.io>
2018-06-05 14:36:41 +0530

    modified main class

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,12 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
